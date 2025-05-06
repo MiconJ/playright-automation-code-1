667084e5c46f2678598a2d53c9a4cd5344c1c56f
MiconJ <109406619+MiconJ@users.noreply.github.com>
2025-05-07 02:51:15 +0900

    first commit

---
+++ tests/UIBasicsTest.spec.js
@@ -0,0 +1,14 @@
+const { test } = require("@playwright/test");
+
+test("First playwirght test", async ({ browser }) => {
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+});
+
+
+test("Open tab using page param", async ({ page }) => {
+  await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+  const title = await page.title();
+  console.log("Page title is:", title);
+});
