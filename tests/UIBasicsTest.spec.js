1f80bed409eec876083626bacc3d34c943f8895c
MiconJ <109406619+MiconJ@users.noreply.github.com>
2025-05-07 03:09:18 +0900

    add comment to clear function, delete example file

--- tests/UIBasicsTest.spec.js
+++ tests/UIBasicsTest.spec.js
@@ -1,13 +1,14 @@
 const { test } = require("@playwright/test");
 
+// Add {browser} to run the test in a new browser context. In function, config context and page to run the test in a new page
 test("First playwright test", async ({ browser }) => {
   const context = await browser.newContext();
   const page = await context.newPage();
   await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
 });
 
-
-test("Open tab using page param", async ({ page }) => {
+// Add .only to run only this test
+test.only("Open tab using page param", async ({ page }) => {
   await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
   const title = await page.title();
   console.log("Page title is:", title);
